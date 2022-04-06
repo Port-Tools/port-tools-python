80e558f1ff4dd6439424ca3477a2542c408e0406
Jimmy <hung.caovu@gmail.com>
2022-04-06 16:29:44 +0700

    add test cases + update logic

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# port-tools-python
+# port.tools python library
+Keep update dynamic ip remotely
+
+Run to perform unit test:
+    python setup.py pytest
+Build library:
+    python setup.py bdist_wheel
+
+Install:
+    pip install /path/to/wheelfile.whl
--- tests/test_sdk.py
+++ tests/test_sdk.py
@@ -1,10 +1,45 @@
-from port_tools.sdk import request
+from time import sleep
+from port_tools.sdk import PortTool
 import unittest
 
 class RequestTest(unittest.TestCase):
+    
     def test_request(self):
-        res = request(id="92B0271E4E")
-        self.assertTrue(
-           res != None
-        )
+        p = PortTool(id="92B0271E4E")
+        # Get info
+        ipv4, ipv6, update, request = p.remote_info
+        assert(ipv4)
+
+    def test_cache_in_expired_time(self):
+        p = PortTool(id="92B0271E4E", cache_time=10)
+        # Get info
+        ipv4, ipv6, update, request1 = p.remote_info
+        ipv4, ipv6, update, request2 = p.remote_info
+        assert(request1 == request2)
         
+    def test_cache_out_expired_time(self):
+        p = PortTool(id="92B0271E4E", cache_time=10)
+        # Get info
+        ipv4, ipv6, update, request1 = p.remote_info
+        sleep(11)
+        ipv4, ipv6, update, request3 = p.remote_info
+        assert(request1 != request3)
+
+    def test_retry(self):
+        PortTool.URL_ENDPOINT = "https://port.tools/api/remote_error"
+        p = PortTool(id="92B0271E4E", retry=10)
+        # Get info
+        try:
+            ipv4, ipv6, update, request1 = p.remote_info
+        except Exception as ex:
+            pass
+
+    def test_use_last_if_error(self):
+        PortTool.URL_ENDPOINT = "https://port.tools/api/remote_error"
+        p = PortTool(id="92B0271E4E", retry=10)
+        # Get info
+        try:
+            p.update()
+        except Exception as ex:
+            pass
+        assert(p.remote_ipv4 == None and p.remote_ipv6 == None and p.remote_last_update == None and p.remote_last_request == None)
